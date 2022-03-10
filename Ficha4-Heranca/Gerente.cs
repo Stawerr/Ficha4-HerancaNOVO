d19db42b1859dc306bbfebe96d6206a6c07147b2
Stawerr <renato-silva_16@hotmail.com>
2022-03-10 12:50:46 +0000

    Add project files.

---
+++ Ficha4-Heranca/Gerente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ficha4_HeritageC_Sharp
+{
+    class Gerente : Funcionario
+    {
+
+        public string Especialidade { get; set; }
+        public int Extensao { get; set; }
+
+        public Gerente() : base()
+        {
+
+        }
+
+        public Gerente(int id, string nome, DateTime dataNasc, string eMail, double valorHora, string especialidade, int extensao) : base(id, nome, dataNasc, eMail, valorHora)
+        {
+            Especialidade = especialidade;
+            Extensao = extensao;
+        }
+        public Gerente(Gerente g) : base(g.Id, g.Nome, g.DataNasc, g.EMail, g.ValorHora)
+        {
+            Especialidade = g.Especialidade;
+            Extensao = g.Extensao;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\t Especialidade: " + Especialidade + "\t Extensão: " + Extensao;
+        }
+        public static void alterarValorHora(List<Funcionario> funcionarios, int op)
+        {
+            int bandeira = 0;
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                if (typeof(Gerente) == funcionarios[i].GetType() && funcionarios[i].Id == op)
+                {
+                    Console.WriteLine("Insira o novo salário");
+                    double sal = double.Parse(Console.ReadLine());
+                    funcionarios[i].ValorHora = sal;
+                    bandeira = 1;
+                }
+            }
+            if (bandeira == 0)
+            {
+                Console.WriteLine("Gerente não encontrado");
+            }
+            Console.ReadKey();
+        }
+        public static int calcularIdade(List<Funcionario> funcionarios, int op)
+        {
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                if (typeof(Gerente) == funcionarios[i].GetType() && funcionarios[i].Id == op)
+                {
+                    if (funcionarios[i].DataNasc.DayOfYear > DateTime.Now.DayOfYear)
+                    {
+                        return DateTime.Now.Year - funcionarios[i].DataNasc.Year - 1;
+                    }
+                    return DateTime.Now.Year - funcionarios[i].DataNasc.Year;
+                }
+            }
+            return 0;
+        }
+
+        public static double calcularSalario(List<Funcionario> funcionarios, int op)
+        {
+            Console.WriteLine("Quantas horas trabalha por dia?");
+            double horas= int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                if (typeof(Gerente) == funcionarios[i].GetType() && funcionarios[i].Id == op)
+                {
+                    horas = horas * funcionarios[i].ValorHora;
+                    double salario= horas * DateTime.DaysInMonth(DateTime.Today.Day,DateTime.Today.Month);
+                    return salario;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Ficha4-Heranca/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ficha4_HeritageC_Sharp
+{
+    class Program
+    {
+        static List<Funcionario> funcionarios;
+        static void Main(string[] args)
+        {
+            funcionarios = new List<Funcionario>();
+            #region Funcs
+            funcionarios.Add(new Gerente(1, "Paulo", new DateTime(1995 , 01 , 16), "paulo@paulo.pt", 32.5, "Caçador", 221597856));
+            funcionarios.Add(new Gerente(2, "Manel", new DateTime(1981 , 05 , 21), "Manel@Manel.pt", 32.5, "Malandro", 22567521));
+            funcionarios.Add(new Gerente(3, "Carla", new DateTime(1975 , 04 , 07), "Carla@Carla.pt", 25.5, "Agricultura", 226597523));
+            funcionarios.Add(new Gerente(4, "Alberto", new DateTime(1997, 09, 26), "Alberto@Alberto.pt", 92.5, "Pesca", 223641579));
+            funcionarios.Add(new Gerente(5, "Joana", new DateTime(1987 , 06 , 18), "Joana@Joana.pt", 32, "Politica", 220357802));
+            funcionarios.Add(new Operario(6, "Joca", new DateTime(1967 , 04 , 16), "Joca@Joca.pt", 50.5, "IT"));
+            funcionarios.Add(new Operario(7, "Ana", new DateTime(1992 , 11 , 10), "Ana@Ana.pt", 26, "Pecuaria"));
+            funcionarios.Add(new Operario(8, "Marta", new DateTime(1989 , 01 , 05), "Marta@Marta.pt", 89.8, "Loja"));
+            funcionarios.Add(new Operario(9, "Vitor", new DateTime(1988 , 12 ,21), "Vitor@Vitor.pt", 10.32, "Comunicações"));
+            #endregion
+            int op = 1;
+            while (op != 0)
+            {
+                
+
+                Console.Clear();
+                Console.WriteLine("1-Inserir Gerente");
+                Console.WriteLine("2-Inserir Operário");
+                Console.WriteLine("3-Visualizar Gerentes");
+                Console.WriteLine("4-Visualizar Operários");
+                Console.WriteLine("5-Visualizar Funcionários");
+                Console.WriteLine("0-Sair");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        inserirGerente();
+                        break;
+                    case 2:
+                        inserirOperario();
+                        break;
+                    case 3:
+                        visualizarGerente();
+                        break;
+                    case 4:
+                        visualizarOperario();
+                        break;
+                    case 5:
+                        VisualizarFuncionarios();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static void inserirGerente()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Insira o ID");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o Nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Insira a data de nascimento: AAAA/MM/DD");
+            DateTime dataNasc = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o E-mail");
+            string eMail = Console.ReadLine();
+            Console.WriteLine("Insira o valor hora");
+            double valorHora = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a Especialidade");
+            string especialidade = Console.ReadLine();
+            Console.WriteLine("Insira a Extensão");
+            int extensao = int.Parse(Console.ReadLine());
+
+            funcionarios.Add(new Gerente(id, nome, dataNasc, eMail, valorHora, especialidade, extensao));
+
+            Console.WriteLine("Gerente inserido com sucesso!");
+            Console.ReadKey();
+        }
+
+        private static void inserirOperario()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Insira o ID");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o Nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Insira a data de nascimento: AAAA/MM/DD");
+            DateTime dataNasc = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o E-mail");
+            string eMail = Console.ReadLine();
+            Console.WriteLine("Insira o valor hora");
+            double valorHora = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o departamento");
+            string departamento = Console.ReadLine();
+
+
+            funcionarios.Add(new Operario(id, nome, dataNasc, eMail, valorHora, departamento));
+
+            Console.WriteLine("Operário inserido com sucesso!");
+            Console.ReadKey();
+        }
+
+        private static void visualizarGerente()
+        {
+
+            Console.Clear();
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                if (typeof(Gerente) == funcionarios[i].GetType())
+                {
+                    Console.WriteLine(funcionarios[i].ToString());
+                }
+            }
+
+            Console.WriteLine("Qual o ID do Gerente a alterar?");
+            int op = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            int op2 = 1;
+            while (op2 != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("1-Alterar Valor Hora");
+                Console.WriteLine("2-Calcular Idade");
+                Console.WriteLine("3-Calcular Salário");
+                Console.WriteLine("0-Sair");
+                op2 = int.Parse(Console.ReadLine());
+
+                switch (op2)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Gerente.alterarValorHora(funcionarios, op);
+                        break;
+                    case 2:
+                        Gerente.calcularIdade(funcionarios, op);
+                        int valor= Gerente.calcularIdade(funcionarios, op);
+                        Console.WriteLine("A idade do gerente é "+ valor);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Gerente.calcularSalario(funcionarios, op);
+                        double salario = Gerente.calcularIdade(funcionarios, op);
+                        Console.WriteLine("O salário do gerente é " + salario);
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void visualizarOperario()
+        {
+            Console.Clear();
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                if (typeof(Operario) == funcionarios[i].GetType())
+                {
+                    Console.WriteLine(funcionarios[i].ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void VisualizarFuncionarios()
+        {
+            Console.Clear();
+            for (int i = 0; i < funcionarios.Count; i++)
+            {
+                {
+                    Console.WriteLine(funcionarios[i].ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
